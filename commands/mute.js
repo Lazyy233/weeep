53cf8cabd4cc942b76f7ac1ac3b19271d1b5fae5
Lazyy233 <51232781+Lazyy233@users.noreply.github.com>
2020-11-15 13:56:26 -0800

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,20 @@
+module.exports.run = async (bot, message, args) => {
+message.delete()
+
+    if(!message.member.hasPermission(['ADMINISTRATOR'])) return;
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
+    if(member.hasPermission(['ADMINISTRATOR']) && !message.member.hasPermission('ADMINISTRATOR')) return;
+    message.delete()
+
+        let mutedRole = message.guild.roles.cache.get('769772710644416547');
+        let verifiedRole = message.guild.roles.cache.get('769772710644416546');
+        if(mutedRole) {
+            member.roles.add(mutedRole);
+            member.roles.remove(verifiedRole);
+            message.channel.send("User was Successfully muted.");
+        }
+}
+
+module.exports.help = {
+  name: "mute"
+}
