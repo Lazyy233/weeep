a78dbac7bba516a6451a0dee20b5f2f2a070385a
Lazyy233 <51232781+Lazyy233@users.noreply.github.com>
2020-11-15 13:59:00 -0800

    Create unmute.js

---
+++ commands/unmute.js
@@ -0,0 +1,19 @@
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission(['ADMINISTRATOR'])) return;
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
+    if(member.hasPermission(['ADMINISTRATOR']) && !message.member.hasPermission('ADMINISTRATOR')) return;
+    
+    message.delete()
+
+        let mutedRole = message.guild.roles.cache.get('769772710644416547');
+        let verifiedRole = message.guild.roles.cache.get('769772710644416546');
+        if(mutedRole) {
+            member.roles.remove(mutedRole);
+            member.roles.add(verifiedRole);
+            message.channel.send("User was Successfully Unmuted.");
+        }
+}
+
+module.exports.help = {
+  name: "unmute"
+}
