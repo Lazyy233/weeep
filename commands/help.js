e1595eb2510dd5a07b719ee4cb1ced46ddfe8ac8
Lazyy233 <51232781+Lazyy233@users.noreply.github.com>
2020-11-15 18:44:02 -0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 message.delete()
 
 message.channel.send("A DM has been sent!")
-message.author.send("**Here are all the commands:** | avatar | ban | botinfo | botinvite | dm | help | kick | lockdown | ping | purge | serverinfo | serverinvite | suicide | unlockdown")
+message.author.send("**Here are all the commands:** | avatar | ban | botinfo | botinvite | dm | help | kick | lockdown | ping | purge | serverinfo | serverinvite | suicide | unlockdown | uptime | mute | unmute | setafk | botstatus |")
   
   
   
